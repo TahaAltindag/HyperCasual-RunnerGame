bf4f9beb30b72d08bb589a557665627d2e70a423
TahaAltindag <taha.altindag223@gmail.com>
2022-06-22 19:33:31 +0300

    Bouncing from rotator sticks are added

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -8,24 +8,47 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] private float swerveSpeed;
     [SerializeField] public GameObject wall;
     Animator animator;
+    Rigidbody rb;
+    private bool isBouncing = false;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         wall.SetActive(false);
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal") * swerveSpeed * Time.deltaTime;
-        this.transform.Translate(horizontal, 0, forwardSpeed * Time.deltaTime);
+        if (!isBouncing)
+        {
+            float horizontal = Input.GetAxis("Horizontal") * swerveSpeed * Time.deltaTime;
+            this.transform.Translate(horizontal, 0, forwardSpeed * Time.deltaTime); 
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
 
-        forwardSpeed = swerveSpeed = 0f;
-        animator.SetBool("isFinished", true);
-        wall.SetActive(true);
+        if (other.tag == "FinishLine")
+        {
+            forwardSpeed = swerveSpeed = 0f;
+            animator.SetBool("isFinished", true);
+            wall.SetActive(true); 
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "stick")
+        {
+            float bounce = 100f;
+            rb.AddForce(collision.contacts[0].normal * bounce);
+            isBouncing = true;
+            Invoke("StopBounce", 0.3f);
+        }
+    }
+    void StopBounce()
+    {
+        isBouncing = false;
     }
 }
